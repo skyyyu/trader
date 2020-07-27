f37ee920ee43cf1ca5a9c570f416dab404bece20
関祐樹 <greenpeace623@docomo.ne.jp>
2020-07-27 09:45:15 +0900

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,4 @@ class User < ApplicationRecord
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
-
-  # def already_liked?(post)
-  #   self.likes.exists?(post_id: post.id)
-  # end
 end
